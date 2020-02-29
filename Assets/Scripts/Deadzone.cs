ec65fc0bec3ab4d4f00db2e96c7dbc50027e946d
William Xie <williamxie168@berkeley.edu>
2020-02-29 01:11:24 -0800

    Initial commit

---
+++ Assets/Scripts/Deadzone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Deadzone : MonoBehaviour {
+
+	public Transform startPoint;
+
+	/**
+	 * OnTriggerEnter2D() function will be called (automatically) by Unity engine when the current game object
+	 * just starts colliding with other game objects. In order to trigger this function call, you need to make
+	 * sure both game objects involved in the collision need to have a Collider2D component attached and is
+	 * a trigger. Rigidbody2D is not required.
+	 */
+	public void OnTriggerEnter2D(Collider2D other) {
+		// If the player hits (or "triggers") the deadzone, we will teleport the player back to the start point.
+		other.transform.position = new Vector3(startPoint.position.x, startPoint.position.y, other.transform.position.z);
+	}
+}
---
+++ Assets/Scripts/EndPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EndPoint : MonoBehaviour {
+
+	public Color reachedColor;
+	
+	[Header("Graphics Settings")]
+	public SpriteRenderer flagRenderer;
+	public SpriteRenderer rodRenderer;
+
+	/**
+	 * OnTriggerEnter2D() function will be called (automatically) by Unity engine when the current game object
+	 * just starts colliding with other game objects. In order to trigger this function call, you need to make
+	 * sure both game objects involved in the collision need to have a Collider2D component attached and the
+	 * Collider2D component for the current game objects need to be a trigger, while the other game object
+	 * needs to have a Rigidbody2D component attached of type "dynamic". Types "Kinematic" or "Static" will
+	 * not trigger this function call.
+	 */
+	public void OnTriggerEnter2D(Collider2D other) {
+		// If the player hits (or "triggers") the end point, we will change its color.
+		flagRenderer.color = reachedColor;
+		rodRenderer.color = reachedColor;
+	}
+}
