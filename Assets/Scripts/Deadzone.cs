e6f8ae75a2ad96f66b1add2c044f53b018a376f9
William Xie <williamxie168@berkeley.edu>
2020-03-08 14:26:32 -0700

    Add BasicPlayerController

--- Assets/Scripts/Deadzone.cs
+++ Assets/Scripts/Deadzone.cs
@@ -9,7 +9,7 @@ public class Deadzone : MonoBehaviour {
 	 * just starts colliding with other game objects. In order to trigger this function call, you need to make
 	 * sure both game objects involved in the collision need to have a Collider2D component attached and is
 	 * a trigger. Rigidbody2D is not required.
-	 */
+	 */	
 	public void OnTriggerEnter2D(Collider2D other) {
 		// If the player hits (or "triggers") the deadzone, we will teleport the player back to the start point.
 		other.transform.position = new Vector3(startPoint.position.x, startPoint.position.y, other.transform.position.z);
