a7e40bbd440d588bf816fb4129225d671d5e2257
jinkang-0 <jinkang.fang@gmail.com>
2022-11-21 15:20:10 -0800

    updated Friend script
    no longer requires manual input of friend id

--- Assets/Scripts/Friend.cs
+++ Assets/Scripts/Friend.cs
@@ -11,33 +11,24 @@ public class Friend : MonoBehaviour
 {
     public static int TotalFriendCount = 0;
     public static int FriendCount = 0;
-    public static Dictionary<string, Dictionary<int, bool>> Friends = new Dictionary<string, Dictionary<int, bool>>();
+    public static Dictionary<string, bool> Friends = new Dictionary<string, bool>();
 
-    [Tooltip("An ID unique to this friend in this scene.")]
-    public int id;
-
+    private string _id;
     private bool _rescued = false;
     
     void Awake()
     {
         var currentScene = SceneManager.GetActiveScene();
-        
+        _id = currentScene.name + gameObject.transform.position.ToString() + gameObject.name;
+
         // ensure current friend is logged & restore logged rescue state
-        if (Friends.ContainsKey(currentScene.name))
+        if (Friends.ContainsKey(_id))
         {
-            if (!Friends[currentScene.name].ContainsKey(id))
-            {
-                Friends[currentScene.name].Add(id, _rescued);
-                TotalFriendCount++;
-            }
-            else
-            {
-                _rescued = Friends[currentScene.name][id];   
-            }
+            _rescued = Friends[_id];
         } 
         else 
         {
-            Friends.Add(currentScene.name, new Dictionary<int, bool>() {{id, _rescued}});
+            Friends.Add(_id, _rescued);
             TotalFriendCount++;
         }
 
@@ -56,7 +47,7 @@ public class Friend : MonoBehaviour
     {
         FriendCount++;
         _rescued = true;
-        Friends[SceneManager.GetActiveScene().name][id] = true;
+        Friends[_id] = true;
         Destroy(gameObject);
         Debug.Log("Current FriendCount: " + FriendCount + "; Current TotalFriendCount: " + TotalFriendCount);
     }
